1324e02ab7bd106c20fdcf083791ed5ef1f9d6ef
Pallagani Ajith kumar <157093907+Pallagani-Ajith-kumar@users.noreply.github.com>
2024-04-23 12:35:15 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,238 @@
+import { Facebook, Twitter } from "@mui/icons-material";
+
+export const Bio = {
+  name: "Pallagani Ajith Kumar",
+  roles: [
+    "Full Stack Developer",
+    
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Pallagani-Ajith-Kumar",
+  resume:
+    "https://drive.google.com/file/d/1spJJxQLxFYFPrynB4UbQ2XTVwF7KJ3zb/view?usp=drive_link",
+  twitter: "https://twitter.com/PallaganiAjith",
+
+  facebook: "https://www.facebook.com/ajith.pallagani?mibextid=ZbWKwL",
+  linkedin: "https://www.linkedin.com/in/ajithkumarpallagani",
+  
+  insta: "https://www.instagram.com/ajith_pallagani/",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+      ,
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Spring Boot",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUKbe3Vg5PJ4wpjlDUy-noAzkT0dqhknQR4TL86jNAKA&s",
+      },
+      
+      
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      
+        
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/300px-GitHub_Invertocat_Logo.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Postman_%28software%29.png/180px-Postman_%28software%29.png",
+      },
+      
+      {
+        name: "Figma",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/600px-Figma-logo.svg.png",
+      },
+    ],
+  },
+];
+
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/c/cb/Bharath_University_Logo.png",
+    school: "Bharath Institute of Higher Education & Research",
+    date: "July 2019 - June 2023",
+    grade: "84 %",
+    desc: "I have completed my Bachelor's degree in Computer Science and Engineering at Bharath University, Chennai, with CGPA of 8.95. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.narayanagroup.com/images/logo.png",
+    school: "Narayana Junior College",
+    date: "july 2017 - March 2019",
+    grade: "90%",
+    desc: "I completed my class 12 high school education at Narayana Junior College, Vijayawada, where I studied Mathtematics Physics & Chemistry.",
+    degree: "ISC(XII), Intermediate",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/47/SV_Logo.jpg",
+    school: "S V Highh School, Agiripalli",
+    date: "Apr 2016 - March 2017",
+    grade: "80%",
+    desc: "I completed my class 10 education at S V High School, Agiripalli. where I studied Basic Mathematics and Languages.",
+    degree: "ICSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Nature Nexus",
+    date: "Dec 2023 - April 2024",
+    description:
+      "The Eco-Volunteering Hub is an online platform connecting passionate travelers with global eco-volunteering opportunities. It enhances community engagement through vibrant forums, global nature events, and NGO collaborations. The user-friendly interface provides real-time updates on projects, and a fundraising platform supports volunteers and projects.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/429px-Altja_j%C3%B5gi_Lahemaal.jpg",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "SQL",
+      "HTML",
+      "CSS",
+      "Bootstrap",
+      "React Js",
+      "IntellJ Idea",
+      "Postman",
+      "Vscode",
+      
+    ],
+    
+  },
+  {
+    id: 0,
+    title: "",
+    date: "Dec 2023 - April 2024",
+    description:
+    "Mob Inspire streamlines the business operations in corporate sector by providing cost-effective and highly productive software solutions.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Restaurant_logo_design_mockup.jpg/330px-Restaurant_logo_design_mockup.jpg",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "SQL",
+      "HTML",
+      "CSS",
+      "Bootstrap",
+      "React Js",
+      "IntellJ Idea",
+      "Postman",
+      "Vscode",
+      
+    ],
+    
+    
+    member: [
+      
+      {
+        name: "Pallagani Ajith Kumar",
+        img: "https://avatars.githubusercontent.com/u/157093907?v=4",
+        linkedin: "https://www.linkedin.com/in/ajithkumarpallagani/",
+        github: "https://github.com/Pallagani-Ajith-Kumar",
+      },
+    ],
+  },
+  
+    
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Completed my Secondary School" },
+  
+  { year: 2019, text: "Completed my Intermediate and Started my B.Tech" },
+  
+  { year: 2023, text: "Completed my B.Tech with 84%" },
+];
